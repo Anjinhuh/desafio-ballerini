c7852e3df03d9c479c2ad0f0ef532917754f7228
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2022-02-09 23:06:25 -0300

    adding archives

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState} from 'react';
+import $ from 'jquery';
 import './App.css';
 
+
+function mostraDiv(){
+  $(".resultado-div").show()
+  console.log("ok")
+}
+document.addEventListener('DOMContentLoaded', function() {
+  $(".resultado-div").hide()
+});
 function App() {
+
+  let [nome, setNome] = useState('NOME NÃO INSERIDO')
+  let [idade, setIdade] = useState('IDADE NÃO INSERIDA')
+  let [tec, setTech] = useState('TECNOLOGIA NÃO INSERIDA')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="app"  >
+      <form onSubmit={(e) =>{e.preventDefault()}}>
+        <div className="div-inputs">
+          <input onChange={(e) => {setNome(e.target.value)} } placeholder="Qual o seu nome?" type="text" />
+          <input onChange={(e) => {setIdade(e.target.value)} } placeholder="Quantos anos vocë tem?" type="text" />
+          <input onChange={(e) => {setTech(e.target.value)} } placeholder="Qual tecnologia você está estudando?" type="text" />
+          <button onClick={() => {mostraDiv()}} >Registrar</button>
+        </div>
+      </form>
+      <div className="resultado-div">
+        <p id='nome'>Nome: {nome}</p>
+        <p id='idade'>Idade: {idade}</p>
+        <p id='tecnologia'>Tecnologia que está estudando: {tec}</p>
+      </div>
     </div>
+
+
   );
 }
 
